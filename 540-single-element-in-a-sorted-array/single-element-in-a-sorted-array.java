1dabfa1b4977ab93fb9f4bf16933b8227629c077
curlmanofficial <158127884+curlmanofficial@users.noreply.github.com>
2026-07-21 11:20:46 +0530

    Time: 0 ms (100.00%) | Memory: 52.8 MB (77.91%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -7,9 +7,9 @@ class Solution {
 
         while(s < e){
              mid = s +(e-s)/2;
-            // if(nums[mid] != nums[mid+1] && nums[mid] != nums[mid-1] ){
-                // return nums[mid];
-            // }
+            if(nums[mid] != nums[mid+1] && nums[mid] != nums[mid-1] ){
+                return nums[mid];
+            }
 
             if((mid & 1) != 0){
                 mid = mid-1;
